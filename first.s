1c671191e0a07872bab723b2ecf8af91ad8728bd
Roshanali-rr <63875910+Roshanali-rr@users.noreply.github.com>
2020-11-08 16:26:15 +0530

    Update first.s

--- first.s
+++ first.s
@@ -1,11 +1,34 @@
-     area     appcode, CODE, READONLY
-     export __main
+ 	AREA     appcode, CODE, READONLY
+     EXPORT __main
 	 ENTRY 
-__main  function		 
-       VLDR.F32   s4, = 0
-       VLDR.F32   s5, = 0 
-       VDIV.F32    s6,s5,s4
-       VLDR.F32   s5, = -1
-       VSQRT.F32 S7, S5	          
-     endfunc
-     end
+__main  FUNCTION		         
+   
+    VLDR.F32   s1, = 5 
+    VLDR.F32   s2, = 5
+    VLDR.F32   s5, = 5 
+    VLDR.F32   s6, = 1	
+    VLDR.F32   S12, = 1          
+
+   MOV R3,#4; N=10 FOR 10 iterations
+   MOV R5,#5;
+   MOV R6,#0 ; r6 and s6 are used as a counter to count 100 iterations 
+	
+LOOP CMP R3,R6
+     BGE LOOP1 ; till 100 iterations it will move to LOOP1 and after that it go to STOP
+     BLE LOOP2
+	 B stop
+	
+LOOP1 ADD R6,R6,#1; r6 and s6 are incremented after every iteration
+      VADD.F32	s6,s6,s12;
+      VMUL.F32 S5,S5,S2;
+	  VDIV.F32 s8,s5,s6;
+	  VADD.F32 S1,S1,S8;
+	  VDIV.F32 s5,s5,s6;
+	  B LOOP;
+
+LOOP2 VADD.F32 S1,S1,S12;
+       B stop
+
+stop B stop 
+   ENDFUNC
+   END
